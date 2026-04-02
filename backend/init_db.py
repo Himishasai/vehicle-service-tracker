4ef0a5fe5891eebb366554cbf20daee6fb7a058a
BOKKA SAI HIMISHA <saihimisha27@gmail.com>
2026-04-03 00:06:38 +0530

    Update init_db.py

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,10 +1,9 @@
 import sqlite3
 
 conn = sqlite3.connect("../database/vehicle.db")
-cur = conn.cursor()
 
 with open("../database/schema.sql") as f:
-    cur.executescript(f.read())
+    conn.executescript(f.read())
 
 conn.commit()
 conn.close()
